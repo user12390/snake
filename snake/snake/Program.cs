335933573ebad79dd711224f331bdd0869f5eedb
user12390 <snovenkikh@inbox.ru>
2018-09-19 14:37:22 +0500

    l7

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -16,26 +16,13 @@ namespace snake
 
             p2.draw();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
+            h_l line = new h_l(5, 10, 8, '+');
+            line.draw();
 
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
 
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
 
-            numList.RemoveAt(0);
 
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
             Console.ReadLine();
         }
-        }
     }
+}
---
+++ snake/snake/h_l.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace snake
+{
+    class h_l
+    {
+        List<Point> pList;
+
+        public h_l(int xLeft, int xRight, int y, char sym)
+        { 
+        pList = new List<Point>();
+
+        for (int x = xLeft; x <= xRight; x++)
+        {
+            Point p = new Point(x, y, sym);
+            pList.Add(p);
+        }
+
+        }
+        public void draw()
+        {
+            foreach (Point p in pList)
+            {
+                p.draw();
+            }
+        }
+
+
+    }
+}
