c74d9ad8695b24be56338a24dca78156baa197ae
user12390 <snovenkikh@inbox.ru>
2018-09-19 13:05:25 +0500

    l6

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -16,8 +16,25 @@ namespace snake
 
             p2.draw();
 
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
 
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
 
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
             Console.ReadLine();
         }
         }
